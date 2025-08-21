dc86bf3705cfd772c7caaf2c65e15c816e861195
Анна Телегина <annatelegina@MacBook-Air-Anna.local>
2025-08-21 23:06:47 +0300

    added clustering and replenishment

---
+++ src/cluster.py
@@ -0,0 +1,63 @@
+import json
+import argparse
+import numpy as np
+import gensim
+from gensim.test.utils import datapath
+from sklearn.cluster import DBSCAN
+from utils import *
+
+def read_json(args):
+    with open(args.input_file) as json_data:
+        input_groups = dict(json.load(json_data))
+    return input_groups
+    
+def main():
+
+    parser = argparse.ArgumentParser(description="Cognates clustering")
+    parser.add_argument("-m", "--model-path", type=str, help="Path to the fasttext model", default='../213/model.model')
+    parser.add_argument( "--model-type", type=str, help="FastText or Word2Vec", default='word2vec')
+    parser.add_argument("-i", '--input-file', type=str, help="Json with input groups", default='filtered_groups_20.json')
+    parser.add_argument("-o", '--output', type=str, help="path to the json output", default='output_clusters.json')
+    parser.add_argument('--min-samples', type=int, help="Min samples argument for DBSCAN", default=3)
+    parser.add_argument('--eps', type=float, help="Eps argument for DBSCAN", default=1.1)
+    args = parser.parse_args()
+    
+    if args.model_type == "fasttext":
+        model = gensim.models.fasttext.FastTextKeyedVectors.load(args.model_path)
+    else: 
+        model = gensim.models.KeyedVectors.load_word2vec_format(datapath(args.model_path), 
+                                                            binary=True)
+    _, file_ext = os.path.splitext(args.input_file)
+
+    if file_ext == ".txt":
+        input_groups = {}
+        input_groups[0] = read_words(args.input_file)
+    elif file_ext == ".json":
+        input_groups = read_json(args)
+    
+    filtered = {}
+    output = {}
+    
+    for i in input_groups.keys():
+        vectors, found_words, missed = get_embeddings(model, input_groups[i], 
+                                        fasttext=(args.model_type == "fasttext"))
+        
+        dbscan = DBSCAN(eps=args.eps, min_samples=args.min_samples)
+        y_pred = dbscan.fit_predict(vectors)
+        
+        sorted_list = {i: [] for i in range(np.min(y_pred), np.max(y_pred) + 1)}
+        
+        for j, w in enumerate(found_words):
+            sorted_list[int(y_pred[j])].append(w)
+
+        sorted_list["missed"] = []
+
+        for w in missed:
+            sorted_list["missed"].append(w)
+        output[i] = sorted_list
+        
+    with open(args.output, 'w') as f:
+        json.dump(output, f)
+            
+if __name__ == "__main__":
+    main()
---
+++ src/replenish.py
@@ -0,0 +1,78 @@
+import json
+import argparse
+import numpy as np
+import gensim
+from gensim.test.utils import datapath
+from sklearn.cluster import DBSCAN
+from utils import *
+import copy
+
+def read_json(args):
+    with open(args.input_file) as json_data:
+        input_groups = dict(json.load(json_data))
+    return input_groups
+
+def get_centroids(model, dataset):
+    embeds = {}
+    for k in dataset.keys():
+        if k == "missed":
+            continue
+        vec = 0
+        n = 0
+        for w in dataset[k]:
+            try:
+                v = model[w]
+                vec += v
+                n += 1
+            except Exception as e:
+                continue
+        if n > 0:
+            embeds[k] = vec/n
+    return embeds
+
+def get_similar(embeds, model, word):
+    try:
+        a = model[word]
+    except Exception as e:
+        print("No such word")
+        return
+    sims = []
+    for k in embeds.keys():
+        b = embeds[k]
+        cos_sim = np.dot(a, b)/(np.linalg.norm(a)*np.linalg.norm(b))
+        sims.append((k, cos_sim))
+
+    sims = sorted(sims, key=lambda x: x[1], reverse=True)
+    return sims
+    
+def main():
+
+    parser = argparse.ArgumentParser(description="Cognates clustering")
+    parser.add_argument("-m", "--model-path", type=str, help="Path to the fasttext model", default='../213/model.model')
+    parser.add_argument("-i", '--input-file', type=str, help="Json with input groups", default='filtered_groups_20.json')
+    parser.add_argument("-o", '--output', type=str, help="path to the json output", default='output_clusters_replenished.json')
+    args = parser.parse_args()
+    
+    model = gensim.models.fasttext.FastTextKeyedVectors.load(args.model_path)
+    input_groups = read_json(args)
+    
+    output = {}
+    
+    for i in input_groups.keys():
+        
+        groups = copy.deepcopy(input_groups[i])
+        centroids = get_centroids(model, input_groups[i])
+        
+        for w in input_groups[i]["missed"]:    
+            sims = get_similar(centroids, model, w)[0][0]
+            groups[sims].append(w)
+
+        groups.pop("missed", None)
+
+        output[i] = groups
+
+    with open(args.output, 'w') as f:
+        json.dump(output, f)
+            
+if __name__ == "__main__":
+    main()
